9ec4d827ef912cfabf595b0f2553cdc4a865cdaa
MetalSnow <aymanetrend@gmail.com>
2025-05-08 19:49:16 +0800

    feat: add validation to the category name and partials folder inside views
    -added validation error when user input none alphabetic in the name
    -created new folder partials inside views to include all reusable components in it

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,21 @@
 const expressAsyncHandler = require('express-async-handler');
 const dbCategories = require('../db/categoryQueries');
 const CustomNotFoundError = require('../errors/CustomNotFoundError');
+const { body, validationResult } = require('express-validator');
+
+const alphaErr = 'must only contain letters.';
+
+const validateUser = [
+  body('category').trim().isAlpha().withMessage(`Category name ${alphaErr}`),
+];
+
+const organizeCategories = (categories) => {
+  const freshMart = categories.filter((item) => item.seller === 'FreshMart');
+  const organicHeaven = categories.filter(
+    (item) => item.seller === 'Organic Heaven'
+  );
+  return { freshMart, organicHeaven };
+};
 
 const getCategories = expressAsyncHandler(async (req, res) => {
   const categories = await dbCategories.getAllCategories();
@@ -9,29 +24,32 @@ const getCategories = expressAsyncHandler(async (req, res) => {
     throw new CustomNotFoundError('No Categories have been found');
   }
 
-  const freshMartCategories = categories.filter(
-    (item) => item.seller === 'FreshMart'
-  );
-  const organicHeavenCategories = categories.filter(
-    (item) => item.seller === 'Organic Heaven'
-  );
-
   res.render('categories', {
     title: 'Grocery Store',
-    categories: {
-      freshMart: freshMartCategories,
-      organicHeaven: organicHeavenCategories,
-    },
+    categories: organizeCategories(categories),
   });
 });
 
-const createCategory = expressAsyncHandler(async (req, res) => {
-  const sellerId = req.body.seller == 'FreshMart' ? 1 : 2;
-  const category = { name: req.body.category, sellerId: sellerId };
+const createCategory = [
+  validateUser,
+  expressAsyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    const categories = await dbCategories.getAllCategories();
 
-  await dbCategories.insertCategory(category);
-  res.redirect('/categories');
-});
+    if (!errors.isEmpty()) {
+      return res.status(400).render('categories', {
+        title: 'Grocery Store',
+        categories: organizeCategories(categories),
+        errors: errors.array(),
+      });
+    }
+    const sellerId = req.body.seller == 'FreshMart' ? 1 : 2;
+    const category = { name: req.body.category, sellerId: sellerId };
+
+    await dbCategories.insertCategory(category);
+    res.redirect('/categories');
+  }),
+];
 
 const deleteCategory = expressAsyncHandler(async (req, res) => {
   const categoryId = req.params.id;
